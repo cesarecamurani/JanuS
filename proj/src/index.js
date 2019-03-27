249d1fef74a08fa7cdb65ea5482f1da252dbebc3
Cesare Camurani <cesare.camurani@gmail.com>
2019-03-27 13:20:41 +0000

    "Judgement message" added

--- proj/src/index.js
+++ proj/src/index.js
@@ -1,13 +1,22 @@
 const colors = require('colors');
 
+const summary = { success: 0, fail: 0, disabled: 0 };
+
 const judge = (title, cb) => {
   try{
     cb();
-    console.log(`${' Passed '.bgGreen.black} ${title.green}`);
+    console.log(`${' OK '.bgGreen.black} ${title.green}`);
+    summary.success++;
   } catch(e) {
-    console.log(`${' Failed '.bgRed.black} ${title.red}`);
+    console.log(`${' FAIL '.bgRed.black} ${title.red}`);
     console.log(e.stack.red);
+    summary.fail++;
   }
+};
+
+const xjudge = (title, cb) => {
+  console.log(`${' DISABLED '.bgWhite.black} ${title.gray}`);
+  summary.disabled++;
 };
 
 const verify = (val) => {
@@ -16,8 +25,15 @@ const verify = (val) => {
   throw new Error('Assertion failed.');
 };
 
-const xjudge = (title, cb) => {
-  console.log(`${' Not Judged '.bgWhite.black} ${title.gray}`);
+const judgement = () => {
+  console.log(`\n.......\n`);
+  console.log('Test summary:\n');
+  console.log(`  Success: ${summary.success}`.green);
+  console.log(`  Fail: ${summary.fail}`.red);
+  console.log(`  Disabled: ${summary.disabled}\n\n`.gray);
+
+  if (summary.fail > 0) process.exit(1);
+  process.exit(0);
 };
 
-module.exports = { verify, judge, xjudge };
+module.exports = { verify, judge, xjudge, judgement };

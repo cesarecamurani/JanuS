0e0aa904a106347a1aab2439e40f56ec1bc85fad
Cesare Camurani <cesare.camurani@gmail.com>
2019-03-28 12:45:14 +0000

    deeplyIdentical matcher changed

--- proj/src/matchers/index.js
+++ proj/src/matchers/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   same: require('./same'),
   identical: require('./identical'),
-  deeplyIdentical: require('./deeply-identical'),
+  deeplyIdentical: require('./deeplyIdentical'),
   contains: require('./contains'),
   notContains: require('./notContains'),
   isFalse: require('./isFalse'),

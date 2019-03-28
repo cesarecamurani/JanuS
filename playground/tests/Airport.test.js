7518e0c9cf2e90e60b9fb583b172ee8dc34c5d58
Cesare Camurani <cesare.camurani@gmail.com>
2019-03-28 11:46:49 +0000

    clearForlanding - clearForTakeOff tests passing

--- playground/tests/Airport.test.js
+++ playground/tests/Airport.test.js
@@ -1,18 +1,18 @@
-// const { verify, group, check  } = require('janus');
-// const airport = require('../src/Airport')
-// const plane = require('../src/Plane')
-//
-// group('Airport', () => {
-//   group('method: clearForLanding', () => {
-//     check('adds a plane to the hangar', () => {
-//       airport.clearForLanding(plane)
-//       verify.contains(airport.hangar, plane);
-//     });
-//   });
-//   group('method: clearForTakeOff', () => {
-//     check('removes a plane from the hangar', () => {
-//       airport.clearForTakeOff(plane)
-//       verify.notContains(airport.hangar, plane);
-//     });
-//   });
-// });
+const { verify, group, check  } = require('janus');
+const airport = require('../src/Airport')
+const plane = require('../src/Plane')
+
+group('Airport', () => {
+  group('method: clearForLanding', () => {
+    check('adds a plane to the hangar', () => {
+      airport.clearForLanding(plane)
+      verify.contains(airport.hangar, plane);
+    });
+  });
+  group('method: clearForTakeOff', () => {
+    check('removes a plane from the hangar', () => {
+      airport.clearForTakeOff(plane)
+      verify.notContains(airport.hangar, plane);
+    });
+  });
+});

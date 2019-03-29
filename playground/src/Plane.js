4fe8587cee0fcd744f84dd8402daf152910e0227
Cesare Camurani <cesare.camurani@gmail.com>
2019-03-29 14:24:36 +0000

    Playground deleted

--- playground/src/Plane.js
+++
@@ -1,9 +0,0 @@
-class Plane {
-
-  constructor(model){
-    this.model = model;
-  }
-}
-
-const plane = new Plane
-module.exports = plane
--- playground/test/index.js
+++
@@ -1,8 +0,0 @@
-const janus = require('janus');
-
-require('./string-utils.test');
-require('./number-utils.test');
-require('./CruiseTicket.test');
-require('./Airport.test');
-
-janus.report();
